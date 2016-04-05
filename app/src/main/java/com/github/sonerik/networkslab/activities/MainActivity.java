a941793419f17e5984ecf0275340fca6843276e4
sonerik <thesonerik@gmail.com>
2016-04-05 21:25:39 +0300

    Fixed wrong onCreate override in MainActivity.java

--- app/src/main/java/com/github/sonerik/networkslab/activities/MainActivity.java
+++ app/src/main/java/com/github/sonerik/networkslab/activities/MainActivity.java
@@ -1,7 +1,6 @@
 package com.github.sonerik.networkslab.activities;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -15,8 +14,8 @@ import org.greenrobot.eventbus.Subscribe;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         getSupportFragmentManager().beginTransaction()

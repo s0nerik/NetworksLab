5dd046c4a8a8d53f60e1e468fbac98c8bc66ccad
sonerik <thesonerik@gmail.com>
2016-04-05 20:38:23 +0300

    Constants.java

---
+++ app/src/main/java/com/github/sonerik/networkslab/Constants.java
@@ -0,0 +1,7 @@
+package com.github.sonerik.networkslab;
+
+public class Constants {
+    public static final String LOG_TAG = "networkslab";
+    public static final String SERVICE_CHAT = "chat";
+    public static final int SERVICE_CHAT_DEFAULT_PORT = 50489;
+}

de8d1d095eab286d52b8ea719513a5a6c3c26b2f
sonerik <thesonerik@gmail.com>
2016-04-10 22:08:17 +0300

    Refactored TicTacToeField

--- app/src/main/java/com/github/sonerik/networkslab/custom_views/TicTacToeField.java
+++ app/src/main/java/com/github/sonerik/networkslab/custom_views/TicTacToeField.java
@@ -9,15 +9,15 @@ import android.widget.TextView;
 import com.github.sonerik.networkslab.R;
 
 public class TicTacToeField extends LinearLayout {
-    private TextView cell11;
-    private TextView cell12;
-    private TextView cell13;
-    private TextView cell21;
-    private TextView cell22;
-    private TextView cell23;
-    private TextView cell31;
-    private TextView cell32;
-    private TextView cell33;
+    public enum CellValue { EMPTY, X, O }
+
+    public interface CellValueChangedListener {
+        void onCellValueChanged(int x, int y, CellValue value);
+    }
+
+    private TextView[][] cells = new TextView[3][3];
+    private CellValue playerCellValue = CellValue.X;
+    private CellValueChangedListener valueChangedListener;
 
     public TicTacToeField(Context context) {
         super(context);
@@ -34,53 +34,48 @@ public class TicTacToeField extends LinearLayout {
         init();
     }
 
-
     private void init() {
         View view = inflate(getContext(), R.layout.tictactoe_field, null);
-        this.cell11 = (TextView) view.findViewById(R.id.cell11);
-        this.cell12 = (TextView) view.findViewById(R.id.cell12);
-        this.cell13 = (TextView) view.findViewById(R.id.cell13);
-        this.cell21 = (TextView) view.findViewById(R.id.cell21);
-        this.cell22 = (TextView) view.findViewById(R.id.cell22);
-        this.cell23 = (TextView) view.findViewById(R.id.cell23);
-        this.cell31 = (TextView) view.findViewById(R.id.cell31);
-        this.cell32 = (TextView) view.findViewById(R.id.cell32);
-        this.cell33 = (TextView) view.findViewById(R.id.cell33);
+        cells[0][0] = (TextView) view.findViewById(R.id.cell11);
+        cells[0][1] = (TextView) view.findViewById(R.id.cell12);
+        cells[0][2] = (TextView) view.findViewById(R.id.cell13);
+        cells[1][0] = (TextView) view.findViewById(R.id.cell21);
+        cells[1][1] = (TextView) view.findViewById(R.id.cell22);
+        cells[1][2] = (TextView) view.findViewById(R.id.cell23);
+        cells[2][0] = (TextView) view.findViewById(R.id.cell31);
+        cells[2][1] = (TextView) view.findViewById(R.id.cell32);
+        cells[2][2] = (TextView) view.findViewById(R.id.cell33);
+
+        for (TextView[] row : cells) {
+            for (TextView cell : row) {
+                cell.setOnClickListener(clickListener);
+            }
+        }
+
         addView(view);
     }
 
-    public void set(int x, int y, boolean value) {
-        String cell = x + "" + y;
-        String xo = value ? "O" : "X";
-        switch (cell) {
-            case "11":
-                cell11.setText(xo);
-                break;
-            case "12":
-                cell12.setText(xo);
-                break;
-            case "13":
-                cell13.setText(xo);
-                break;
-            case "21":
-                cell21.setText(xo);
-                break;
-            case "22":
-                cell22.setText(xo);
-                break;
-            case "23":
-                cell23.setText(xo);
-                break;
-            case "31":
-                cell31.setText(xo);
-                break;
-            case "32":
-                cell32.setText(xo);
-                break;
-            case "33":
-                cell33.setText(xo);
-                break;
-            default:
-        }
+    private View.OnClickListener clickListener = v -> {
+        int x = Character.getNumericValue(v.getTag().toString().charAt(0));
+        int y = Character.getNumericValue(v.getTag().toString().charAt(1));
+        boolean isEmpty = cells[x][y].getText().length() == 0;
+
+        set(x, y, isEmpty ? playerCellValue : CellValue.EMPTY);
+    };
+
+    private void set(int x, int y, CellValue value) {
+        cells[x][y].setText(value == CellValue.EMPTY ? "" : value.toString());
+
+        if (valueChangedListener != null)
+            valueChangedListener.onCellValueChanged(x, y, value);
     }
+
+    public void setPlayerCellValue(CellValue playerCellValue) {
+        this.playerCellValue = playerCellValue;
+    }
+
+    public void setCellValueChangedListener(CellValueChangedListener valueChangedListener) {
+        this.valueChangedListener = valueChangedListener;
+    }
+
 }
--- app/src/main/java/com/github/sonerik/networkslab/fragments/TicTacToeFragment.java
+++ app/src/main/java/com/github/sonerik/networkslab/fragments/TicTacToeFragment.java
@@ -3,43 +3,21 @@ package com.github.sonerik.networkslab.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
+import com.github.sonerik.networkslab.Constants;
 import com.github.sonerik.networkslab.R;
 import com.github.sonerik.networkslab.custom_views.TicTacToeField;
-import com.github.sonerik.networkslab.events.OnCellClickedEvent;
-
-import org.greenrobot.eventbus.EventBus;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class TicTacToeFragment extends Fragment {
-
     @Bind(R.id.field)
     TicTacToeField field;
-
-    @Bind(R.id.cell11)
-    TextView cell11;
-    @Bind(R.id.cell12)
-    TextView cell12;
-    @Bind(R.id.cell13)
-    TextView cell13;
-    @Bind(R.id.cell21)
-    TextView cell21;
-    @Bind(R.id.cell22)
-    TextView cell22;
-    @Bind(R.id.cell23)
-    TextView cell23;
-    @Bind(R.id.cell31)
-    TextView cell31;
-    @Bind(R.id.cell32)
-    TextView cell32;
-    @Bind(R.id.cell33)
-    TextView cell33;
 
     @Nullable
     @Override
@@ -52,73 +30,9 @@ public class TicTacToeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         ButterKnife.bind(this, view);
 
-        cell11.setOnClickListener(clickListener);
-        cell12.setOnClickListener(clickListener);
-        cell13.setOnClickListener(clickListener);
-        cell21.setOnClickListener(clickListener);
-        cell22.setOnClickListener(clickListener);
-        cell23.setOnClickListener(clickListener);
-        cell31.setOnClickListener(clickListener);
-        cell32.setOnClickListener(clickListener);
-        cell33.setOnClickListener(clickListener);
+        field.setPlayerCellValue(TicTacToeField.CellValue.O);
+        field.setCellValueChangedListener((x, y, value) -> Log.d(Constants.LOG_TAG, "Value changed: ("+x+","+y+"), "+value));
     }
-
-    private View.OnClickListener clickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            int x = 0;
-            int y = 0;
-            boolean isEmpty = true;
-            switch (v.getId()) {
-                case R.id.cell11:
-                    x = 1;
-                    y = 1;
-                    isEmpty = cell11.getText().equals("");
-                    break;
-                case R.id.cell12:
-                    x = 1;
-                    y = 2;
-                    isEmpty = cell12.getText().equals("");
-                    break;
-                case R.id.cell13:
-                    x = 1;
-                    y = 3;
-                    isEmpty = cell13.getText().equals("");
-                    break;
-                case R.id.cell21:
-                    x = 2;
-                    y = 1;
-                    isEmpty = cell21.getText().equals("");
-                    break;
-                case R.id.cell22:
-                    x = 2;
-                    y = 2;
-                    isEmpty = cell22.getText().equals("");
-                    break;
-                case R.id.cell23:
-                    x = 2;
-                    y = 3;
-                    isEmpty = cell23.getText().equals("");
-                    break;
-                case R.id.cell31:
-                    x = 3;
-                    y = 1;
-                    isEmpty = cell31.getText().equals("");
-                    break;
-                case R.id.cell32:
-                    x = 3;
-                    y = 2;
-                    isEmpty = cell32.getText().equals("");
-                    break;
-                case R.id.cell33:
-                    x = 3;
-                    y = 3;
-                    isEmpty = cell33.getText().equals("");
-                    break;
-            }
-            EventBus.getDefault().post(new OnCellClickedEvent(x, y, isEmpty));
-        }
-    };
 
     @Override
     public void onDestroyView() {

2f7354d7c21cd2ccfb16fdf56c7d9b71436b8674
Alex Isaienko <sonerik@mail.ua>
2016-04-11 16:56:44 +0300

    Made TicTacToeFragment.java abstract

--- app/src/main/java/com/github/sonerik/networkslab/Constants.java
+++ app/src/main/java/com/github/sonerik/networkslab/Constants.java
@@ -2,7 +2,11 @@ package com.github.sonerik.networkslab;
 
 public class Constants {
     public static final String LOG_TAG = "networkslab";
-    public static final String SERVICE_CHAT = "chat";
     public static final String FRAGMENT_DEVICE_CHOOSER = "device_chooser";
-    public static final int SERVICE_CHAT_DEFAULT_PORT = 50489;
+
+    public static final String  SERVICE_CHAT = "chat";
+    public static final int     SERVICE_CHAT_DEFAULT_PORT = 50489;
+
+    public static final String  SERVICE_TIC_TAC_TOE = "tic_tac_toe";
+    public static final int     SERVICE_TIC_TAC_TOE_DEFAULT_PORT = 50489;
 }
--- app/src/main/java/com/github/sonerik/networkslab/fragments/tic_tac_toe/TicTacToeFragment.java
+++ app/src/main/java/com/github/sonerik/networkslab/fragments/tic_tac_toe/TicTacToeFragment.java
@@ -11,11 +11,12 @@ import android.view.ViewGroup;
 import com.github.sonerik.networkslab.Constants;
 import com.github.sonerik.networkslab.R;
 import com.github.sonerik.networkslab.custom_views.TicTacToeField;
+import com.github.sonerik.networkslab.fragments.base.NetworkFragment;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class TicTacToeFragment extends Fragment {
+public abstract class TicTacToeFragment extends NetworkFragment {
     @Bind(R.id.field)
     TicTacToeField field;
 
@@ -30,8 +31,8 @@ public class TicTacToeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         ButterKnife.bind(this, view);
 
-        field.setPlayerCellValue(TicTacToeField.CellValue.O);
-        field.setCellValueChangedListener((x, y, value) -> Log.d(Constants.LOG_TAG, "Value changed: ("+x+","+y+"), "+value));
+        field.setPlayerCellValue(getPlayerCellValue());
+        field.setCellValueChangedListener(this::onCellValueChanged);
     }
 
     @Override
@@ -40,4 +41,17 @@ public class TicTacToeFragment extends Fragment {
         super.onDestroyView();
     }
 
+    @Override
+    protected String getServiceName() {
+        return Constants.SERVICE_TIC_TAC_TOE;
+    }
+
+    @Override
+    protected int getDefaultServicePort() {
+        return Constants.SERVICE_TIC_TAC_TOE_DEFAULT_PORT;
+    }
+
+    protected abstract TicTacToeField.CellValue getPlayerCellValue();
+
+    protected abstract void onCellValueChanged(int x, int y, TicTacToeField.CellValue v);
 }
